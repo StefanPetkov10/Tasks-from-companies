6a5b946b0d83877d152560cfb9f118b8e92e6b1f
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2025-04-27 15:56:08 +0300

    Add task

---
+++ Codexio/test01/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace test01
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int EmployeeCount { get; set; }
+        public Employee(string name, int employeeCount)
+        {
+            Name = name;
+            EmployeeCount = employeeCount;
+        }
+    }
+}
---
+++ Codexio/test01/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace test01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee("John", 3));
+            employees.Add(new Employee("Peter", 2));
+            employees.Add(new Employee("Mark", 0));
+            employees.Add(new Employee("Elain", 0));
+            employees.Add(new Employee("Maria", 1));
+            employees.Add(new Employee("Larra", 1));
+            employees.Add(new Employee("Ana", 0));
+            employees.Add(new Employee("George", 0));
+
+            /* 
+             John
+               Peter
+                 Mark
+                 Elain
+               Maria
+                 Larra
+                   Ana
+               George
+             */
+            Console.WriteLine(employees[0].Name);
+            int indent = 1;
+            int count = 0;
+
+            for (int i = 1; i < employees.Count; i++)
+            {
+                if (employees[i].EmployeeCount > 0)
+                {
+                    Console.WriteLine(new string(' ', indent * 2) + employees[i].Name);
+                    indent++;
+                    count += employees[i].EmployeeCount;
+                }
+
+                else if (employees[i].EmployeeCount == 0)
+                {
+                    Console.WriteLine(new string(' ', indent * 2) + employees[i].Name);
+                    count--;
+                }
+
+                if (count == 0)
+                {
+                    indent--;
+                }
+
+            }
+        }
+    }
+}
