47826b74bbddf7ecb10696eee1fcc56c858bf010
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2025-04-27 15:58:57 +0300

    Add task

---
+++ Amdaris/Test1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Tests
+{
+    class Program
+    {
+        static void Main()
+        {
+            string order = "milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza";
+            string formattedOrder = FormatOrder(order);
+            Console.WriteLine(formattedOrder);
+        }
+
+        static string FormatOrder(string order)
+        {
+            List<string> menu = new List<string> { "Pizza", "Sandwich", "Burger", "Fries", "Chicken", "Onionrings", "Milkshake", "Coke" };
+            List<string> result = new List<string>();
+
+            int i = 0;
+            while (i < order.Length)
+            {
+                foreach (var item in menu)
+                {
+                    if (order.Substring(i).StartsWith(item.ToLower()))
+                    {
+                        result.Add(item);
+                        i += item.Length;
+                        break;
+                    }
+                }
+            }
+
+            return string.Join(" ", result);
+        }
+    }
+}
