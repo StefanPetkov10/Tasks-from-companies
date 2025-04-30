ad98fe500cc924212dce7f96d9232f261a0a05ea
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2025-04-30 10:09:08 +0300

    Add task

---
+++ Progress/Test1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Test1
+{
+    class Program
+    {
+        static int ValidatePassSentences(List<string> passSentences)
+        {
+            int validCount = 0;
+            foreach (string sentence in passSentences)
+            {
+                string[] words = sentence.Split();
+                HashSet<string> uniqueWords = new HashSet<string>(words);
+                if (words.Length == uniqueWords.Count)
+                {
+                    validCount++;
+                }
+            }
+            return validCount;
+        }
+
+        static void Main(string[] args)
+        {
+            List<string> passSentences = new List<string>
+        {
+            "aa bb cc dd ee",
+            "aa bb cc dd aa",
+            "aa bb cc dd aaa"
+        };
+            int result = ValidatePassSentences(passSentences);
+            Console.WriteLine(result); // Output: 2
+        }
+    }
+
+}
