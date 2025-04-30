bf05eb27bb8ac8c56bd1e30a5bd83e2c76bc9092
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2025-04-30 10:09:35 +0300

    Add task

---
+++ Progress/Test4/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Test4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = "123";
+            string text = "aaa321aaa";
+            int count = CountPatternOccurrences(pattern, text);
+            Console.WriteLine("Count: " + count); // Output: Count: 1
+        }
+
+        static int CountPatternOccurrences(string pattern, string text)
+        {
+            int patternLength = pattern.Length;
+            int textLength = text.Length;
+            int count = 0;
+
+            for (int i = 0; i <= textLength - patternLength; i++)
+            {
+                if (text.Substring(i, patternLength) == pattern)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+
+}
